8a7714a77cd2522048fc805ad2ce59337504f6ba
Katie B <kblatherwick@productscape.tech>
2024-03-27 08:47:47 -0600

    before deploy commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,42 @@
+import { useState } from "react";
 import "./App.css";
+import { puppyList } from "./data";
 
-// return (
-//   <>
-<div className="card"></div>;
-//   </>
-// );
+//-------- log the list
+
+const App = () => {
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
+  return (
+    <>
+      {/* map over puppies and return name using map statement */}
+      <div>
+        {puppies.map((puppy) => {
+          return (
+            <p
+              onClick={() => {
+                setFeatPupId(puppy.id);
+              }}
+              key={puppy.id}
+            >
+              {puppy.name}
+            </p>
+          );
+        })}
+        {featPupId && (
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
 export default App;
